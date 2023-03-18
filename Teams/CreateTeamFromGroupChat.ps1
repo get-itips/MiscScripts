e5e7de3819deffcad9a7ec4f051432013aae4ff7
Andres Mariano Gorzelany <36666927+get-itips@users.noreply.github.com>
2023-03-18 08:02:31 -0600

    Update CreateTeamFromGroupChat.ps1

--- Teams/CreateTeamFromGroupChat.ps1
+++ Teams/CreateTeamFromGroupChat.ps1
@@ -1,23 +1,43 @@
+# Author: Andrés Gorzelany
+
+   <# 
+    .SYNOPSIS
+        Creates a Microsoft Teams team from a Group Chat.
+    .DESCRIPTION
+        Creates a Microsoft Teams team from a Group Chat that you are part of, with the same members.
+	It requires Graph PowerShell and Microsoft Teams PowerShell module.
+    .EXAMPLE
+        CreateTeamFromGroupChat.ps1
+    #>
+
+#Connect to Graph
 Connect-MgGraph -Scopes "User.Read.All", "Group.ReadWrite.All,Chat.Read,Chat.ReadWrite,ChatMember.ReadWrite,Chat.ReadBasic"
 
-Read-Host "Enter your email address"
-Get-MgUserChat -UserId admin@M365x94367470.OnMicrosoft.com
+$sipURI=Read-Host "Enter your SIP URI"
+Write-Host "Entered user has the following active chats"
+Write-Host "-------------------------------------------"
+Get-MgUserChat -UserId $sipURI2
+Write-Host "To better identify chats, open teams web, browse to chats and look the URL for the desired chat." -ForegroundColor Green
 
-$groupChat=Read-Host "Copy the desired group chat from the list and paste it here"
+$groupChat=Read-Host "Copy the desired group id chat from the list and paste it here:"
 
-Write-Host "Listing members of the Group Chat"
+Write-Host "Listing members of the entered Group Chat" -ForegroundColor Green
 
-$members=Get-MgUserChatMember -ChatId $groupChat -UserId admin@M365x94367470.OnMicrosoft.com
+$members=Get-MgUserChatMember -ChatId $groupChat -UserId $sipURI
 
 $memberProperties=$members.AdditionalProperties
 foreach($mp in $memberproperties){$mp.email}
 
+Write-Host "Done, now connecting to Teams PowerShell..." -ForegroundColor Green
+
 Connect-MicrosoftTeams
 
 $newTeamName=Read-Host "Name your new Team"
+$newTeamNick=$newTeamName -replace " ",""
+$newTeamVisibility=Read-Host "Enter public or private"
 $group = New-Team -displayname $newTeamName
 
 foreach($mp in $memberproperties){
 	
-	Add-TeamUser -GroupId $group.GroupId -User $mp.email
+	Add-TeamUser -GroupId $group.GroupId -User $mp.email -MailNickname $newTeamNick -visibility $newTeamVisibility
 	}
